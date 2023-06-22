4f0b05906ab160b1e17e23bd970eab6bf351c740
logchan <logchan@outlook.com>
2023-06-21 22:46:28 -0400

    improve info view

--- src/view/InfoView.tsx
+++ src/view/InfoView.tsx
@@ -1,11 +1,14 @@
 import { Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import ReleaseNotes from "../model/ReleaseNotes";
+import { get_nocache_param } from "../utils/DateUtils";
+import { useTranslation } from "react-i18next";
 
 const release_notes = require("../release_notes.json") as ReleaseNotes[];
 
 export default function InfoView() {
   const [dataVersions, setDataVersions] = useState<string[]>([]);
+  const { t } = useTranslation();
 
   const link = (title: string, url: string) => (
     <a href={url} target="_blank" rel="noreferrer">{title}</a>
@@ -17,24 +20,31 @@ export default function InfoView() {
     }
 
     (async () => {
-      const data = await (await fetch("/resources/versions.json")).json();
+      const data = await (await fetch(`/resources/versions.json?${get_nocache_param()}`)).json();
       setDataVersions(
         Object.keys(data).sort().map(k => `${k}: ${data[k]}`)
       );
     })();
   }, [dataVersions]);
 
+  const makeTitle = (s: string) => (
+    <Typography variant="h6">{s}</Typography>
+  );
+
   return (
-    <Stack spacing={1}>
+    <Stack spacing="4px">
       <Typography variant="h5">Closure Talk</Typography>
       <div>
+      {makeTitle(t("latest-update"))}
         <pre>v{release_notes[0].version} ({release_notes[0].date})</pre>
         <pre>{release_notes[0].note}</pre>
+        {makeTitle(t("data-version"))}
         <pre>{dataVersions.join("\n")}</pre>
       </div>
+      {makeTitle(t("previous-updates"))}
       {release_notes.map((item, idx) => {
         if (idx === 0) {
-          return (<div key={idx}>Old Versions</div>);
+          return null;
         }
         return (
           <div key={idx}>
